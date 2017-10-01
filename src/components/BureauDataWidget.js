c2aa12ccd90f6fd5abf6e697e0e257045b93678d
Ethan Bennett <ebennett19@gmail.com>
2017-10-01 15:48:08 -0600

    Add react-md cards

--- src/components/BureauDataWidget.js
+++ src/components/BureauDataWidget.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import Card from 'react-md/lib/Cards/Card';
+import CardTitle from 'react-md/lib/Cards/CardTitle';
+import CardText from 'react-md/lib/Cards/CardText';
 
 import '../../src/assets/stylesheets/BureauDataWidget.scss';
 
 const BureauDataWidget = () => {
-  return <h4>Here's where the Bureau Stats will go</h4>;
+  return (
+    <div>
+      <Card className="bureau-data-widget__card">
+        <CardTitle title="Bureau Data" />
+        <CardText>
+          <p>Here's where the Bureau Stats will go</p>
+        </CardText>
+      </Card>
+    </div>
+  );
 };
 
 export default BureauDataWidget;
--- src/components/ProposalWidget.js
+++ src/components/ProposalWidget.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import Card from 'react-md/lib/Cards/Card';
+import CardTitle from 'react-md/lib/Cards/CardTitle';
+import CardText from 'react-md/lib/Cards/CardText';
 
 import '../../src/assets/stylesheets/ProposalWidget.scss';
 
 const ProposalWidget = () => {
-  return <h4>Here's where the Proposals will go</h4>;
+  return (
+    <div>
+      <Card className="proposal-widget__card">
+        <CardTitle title="Proposals" />
+        <CardText>
+          <p>Here's where the Proposals will go</p>
+        </CardText>
+      </Card>
+    </div>
+  );
 };
 
 export default ProposalWidget;
