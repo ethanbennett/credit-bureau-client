c12ae1943a7177cfc545afb74999845cd810bd2c
Ethan Bennett <ebennett19@gmail.com>
2018-01-14 21:41:41 -0700

    Add console log for debugging

--- src/ethereum/connection.js
+++ src/ethereum/connection.js
@@ -2,6 +2,7 @@ import Web3 from 'web3';
 
 const { REACT_APP_ETH_URL, REACT_APP_DEFAULT_ACCOUNT } = process.env;
 
+console.log(REACT_APP_ETH_URL);
 export const ethereumClient = new Web3(
   new Web3.providers.HttpProvider(REACT_APP_ETH_URL)
 );
