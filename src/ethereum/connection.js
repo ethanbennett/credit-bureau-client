6a4c41ac4ff173595da19c2d7276a2cf7145c0e4
Ethan Bennett <ebennett19@gmail.com>
2018-01-14 21:45:48 -0700

    Remove console log

--- src/ethereum/connection.js
+++ src/ethereum/connection.js
@@ -2,7 +2,6 @@ import Web3 from 'web3';
 
 const { REACT_APP_ETH_URL, REACT_APP_DEFAULT_ACCOUNT } = process.env;
 
-console.log(REACT_APP_ETH_URL);
 export const ethereumClient = new Web3(
   new Web3.providers.HttpProvider(REACT_APP_ETH_URL)
 );
